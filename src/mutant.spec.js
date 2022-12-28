5e3a405160deb873725e30f7202cd3392be721aa
Mikee Ramire <mikee.rmz@gmail.com>
2022-12-28 00:37:56 -0600

    feat: Add new test cases to test the new implementation
    Signed-off-by: Mikee Ramire <mikee.rmz@gmail.com>

--- src/mutant.spec.js
+++ src/mutant.spec.js
@@ -1,50 +1,25 @@
-import isMutant from "./isMutant.js";
+import MutantDetector from "./isMutant.js";
 
-test("isMutant function should return false for non-mutants", () => {
-  expect(
-    isMutant(["ATGCGA", "CAGTGC", "TTATTT", "AGACGG", " GCGTCA", " TCACTG"]) // humano
-  ).toBe(false);
+test("Invalid Characters", () => {
+  const seq = [ "MAATG", "CAG", "TTA" ]
+  const result = new MutantDetector(seq).detectIsMutant();
+  expect(result).toBe('Invalid Character');
 });
 
-test("isMutant function should return true for mutants with horizontal sequences", () => {
-  expect(
-    isMutant(["ATGCGA", "CAGTGC", "TTATGT", "AGACGG", "CCCCTA", "TCACTG"]) // mutante
-  ).toBe(true);
+test("Invalid Sequence", () => {
+  const seq = [ "ATG", "CAG", "TTA" ]
+  const result = new MutantDetector(seq).detectIsMutant();
+  expect(result).toBe('Invalid Sequence');
 });
 
-test("isMutant function should return true for mutants with vertical sequences", () => {
-  expect(
-    isMutant(["ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG"])
-  ).toBe(true);
+test("Return true if the dna is mutant", () => {
+  const seq = ["ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG"];
+  const result = new MutantDetector(seq).detectIsMutant();
+  expect(result).toBe(true);
 });
 
-test("isMutant function should return true for mutants with diagonal sequences", () => {
-  expect(
-    isMutant(["ATGCGA", "CAGTGC", "TTATGT", "AGACGG", "CCTCTA", "TCACTG"])
-  ).toBe(true);
-});
-
-test("isMutant function should return true for mutants with multiple sequences este es de esto", () => {
-  expect(
-    isMutant(["ATGCGA", "CAGTGC", "TTATTT", "AGAAGG", "CCTCTA", "TCACTG"])
-  ).toBe(true);
-});
-
-test("isMutant from the test", () => {
-  expect(
-    isMutant(["ATGCGA", "CAGTGC", "TTATTT", "AGAAGG", "CCTCTA", "TCACTG"])
-  ).toBe(true);
-});
-
-test("isMutant function should return false for invalid DNA tables", () => {
-  expect(
-    isMutant(["ATGCGA", "CAGTGC", "TTATGT", "AGACG", "CCCCTA", "TCACTG"])
-  ).toBe(false);
-  expect(
-    isMutant(["ATGCGA", "CAGTGC", "TTATGT", "AGAXGG", "CCCCTA", "TCACTG"])
-  ).toBe(false);
-  expect(isMutant(["ATGCGA", "CAGTGC", "TTATGT", "", "CCCCTA", "TCACTG"])).toBe(
-    false
-  );
-  expect(isMutant(["ATGCGA", "CAGTGC", "TTATGT"])).toBe(false);
+test("Return false if the dna is non-mutant", () => {
+  const seq = ["ATGCGA", "CAGTGC", "TTATTT", "AGACGG", "GCGTCA", "TCACTG"];
+  const result = new MutantDetector(seq).detectIsMutant();
+  expect(result).toBe(false);
 });
